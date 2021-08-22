db0b64b11130864076d732e0cd16b759a95c90c4
kanwchan <kanwchan0@gmail.com>
2021-08-22 11:41:09 -0400

    created mouseover effect

--- script.js
+++ script.js
@@ -6,8 +6,24 @@ function createGrid(row,column) {
         
         const square= document.createElement('div');
         square.classList.add('gridItem');
-        square.textContent = 'Hello you'
+        square.setAttribute('id','cell')
+        square.textContent = 'Hello kitty'
        container.appendChild(square);
     }
 }
 createGrid(16,16);
+
+
+
+const cells= document.getElementById('cell');
+
+cells.addEventListener("mouseover", func);
+cells.addEventListener("mouseout", func1);
+
+function func() {
+    cells.setAttribute("style", "background-color: black;")
+}
+
+function func1() {
+    cells.setAttribute("style", "background-color: plum")
+}
